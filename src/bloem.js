1ce000678b6d83fa80c7d954b95a550ec753c80f
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-01-01 15:11:15 +0900

    update copyrights

--- src/bloem.js
+++ src/bloem.js
@@ -1,6 +1,6 @@
 // bloem.js - Processing events and actions like Gardening in JavaScript.
 
-// Copyright 2014 TSUYUSATO Kitsune 
+// Copyright 2014-2015 TSUYUSATO Kitsune 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- test/bloem.js
+++ test/bloem.js
@@ -1,4 +1,4 @@
-// Copyright 2014 TSUYUSATO Kitsune 
+// Copyright 2014-2015 TSUYUSATO Kitsune 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
