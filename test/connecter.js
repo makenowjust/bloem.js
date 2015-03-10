76b9f2ec0198e6066d3e2d67e4e52949a6a7f7c6
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-03-10 11:35:54 +0900

    add pending tests

--- test/connecter.js
+++ test/connecter.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2015 TSUYUSATO Kitsune 
+// Copyright 2014-2015 TSUYUSATO Kitsune
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License. 
+// limitations under the License.
 
 'use strict';
 
@@ -29,8 +29,8 @@ describe('bloem', function () {
       }).to.throw(Error);
     });
 
-    function clazz() {
-      bloem.Connecter.call(this);
+    function clazz(head, tail) {
+      bloem.Connecter.call(this, head, tail);
     }
     bloem.Connecter.inherits(clazz);
 
@@ -71,6 +71,10 @@ describe('bloem', function () {
         inst.connect(uniq);
         expect(getTargets(inst)).to.have.length(1).and.include(uniq);
       });
+
+      it('should be added target into head.');
+
+      it('should add target into tail.');
 
       function getTargets(inst) {
         return inst._targets;
